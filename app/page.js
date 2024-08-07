3aa0d5ffd342108c98a42fb218445f66255757bb
Ashley Van't Hoogerhuijs <ashleyjanevth@gmail.com>
2024-08-07 01:06:36 +0100

    add image transitions

--- app/page.js
+++ app/page.js
@@ -1,8 +1,50 @@
 'use client';
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid, Heading } from '@chakra-ui/react';
 import Image from 'next/image';
 import { useState } from 'react';
 import { useBreakpointValue } from '@chakra-ui/react';
+
+const Logo = ({ setIsHovered }) => (
+	<Box
+		height="150"
+		position="absolute"
+		top="270"
+		cursor="pointer"
+		onMouseOver={() => setIsHovered(true)}
+		onMouseOut={() => setIsHovered(false)}
+		zIndex="1"
+	>
+		<Image
+			src="/images/logo.png"
+			width="800"
+			height="450"
+			alt="Will Edgecombe Logo"
+		/>
+	</Box>
+);
+
+const DesktopMenu = () => (
+	<Box
+		position="absolute"
+		top="0"
+		left="0"
+		width="100%"
+		height="100%"
+		backgroundColor="#FFF456"
+		display="flex"
+		justifyContent="center"
+		alignItems="center"
+		zIndex="10"
+		pointerEvents="none"
+	>
+		<Box textAlign="center">
+			<Heading marginBottom="8" size="3xl">
+				INFO
+			</Heading>
+			<Heading size="3xl"> CONTACT </Heading>
+		</Box>
+	</Box>
+);
 
 const DesktopDisplay = () => {
 	const [isHovered, setIsHovered] = useState(false);
@@ -11,53 +53,31 @@ const DesktopDisplay = () => {
 
 	return (
 		<>
-			<Box
-				height="150"
-				position="absolute"
-				top="270"
-				cursor="pointer"
-				onMouseOver={() => setIsHovered(true)}
-				onMouseOut={() => setIsHovered(false)}
-			>
-				<Image
-					src="/images/logo.png"
-					width="800"
-					height="450"
-					alt="Will Edgecombe Logo"
-				/>
-			</Box>
-			{isHovered ? (
-				<Box
-					position="absolute"
-					top="0"
-					left="0"
-					width="100%"
-					height="100%"
-					backgroundColor="#FFF456"
-					display="flex"
-					justifyContent="center"
-					alignItems="center"
-					zIndex="10"
-					pointerEvents="none"
-				>
-					<Box textAlign="center">
-						<Box color="black" fontSize="2xl">
-							Info
-						</Box>
-						<Box color="black" fontSize="2xl">
-							Contact
-						</Box>
-					</Box>
-				</Box>
-			) : null}
+			<Logo setIsHovered={setIsHovered} />
+			{isHovered ? <DesktopMenu /> : null}
 			<SimpleGrid columns="9" spacing="0">
 				{images.map((num) => (
-					<Box key={num}>
+					<Box
+						key={num}
+						position="relative"
+						overflow="hidden"
+						_hover={{
+							opacity: 1,
+							transform: 'scale(3, 3)',
+							zIndex: 1,
+							margin: '20px',
+							overflow: 'visable',
+						}}
+						opacity={0.5}
+						transition="opacity 0.3s ease, transform 0.3s ease"
+						cursor="pointer"
+					>
 						<Image
 							src={`/images/desktop/${num}_website_desktop.jpg`}
 							width="400"
 							height="400"
 							alt="Will Edgecombe Photography"
+							layout="responsive"
 						/>
 					</Box>
 				))}
